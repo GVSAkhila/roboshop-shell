dd5c5ee6d0bd949c8e01bdf2e23fea44daa0071d
sankar <sankargv17999@gmail.com>
2024-05-06 11:46:42 +0530

    installing catalogue roboshop project

--- catalogue.sh
+++ catalogue.sh
@@ -1,91 +1,91 @@
-#!/bin/bash
+# our program goal is to install mongodb
 
-# Define variables
 DATE=$(date +%F)
 LOGDIR=/tmp
 SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGDIR/$0-$DATE.log
 USERID=$(id -u)
 
-# Define colors
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-# Check if script is run as root
+ 
+
 if [ $USERID -ne 0 ]; then
-    echo -e "${R}ERROR:${N} Please run this script with root access"
+    echo "ERROR:: Please run this script with root access"
     exit 1
 fi
 
-# Function to validate command execution
+# this function should validate the previous command and inform user it is success or failure
 VALIDATE() {
+    #$1 --> it will receive the argument1
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ... ${R}FAILURE${N}"
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
     else
-        echo -e "$2 ... ${G}SUCCESS${N}"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-# Set up NPM resource
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
-VALIDATE $? "Setting up NPM resource"
 
-# Install Node.js
+VALIDATE $? "Setting up NPM resource" 
+
 yum install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing Node.js"
 
-# Create user 'roboshop' (assuming it doesn't already exist)
+VALIDATE $? "INstalling nodejs"
+#once user is created , if run the script 2nd time
+#this cammand is fail defanatility
+#improve then script 
 useradd roboshop &>>$LOGFILE
-VALIDATE $? "Creating user 'roboshop'"
 
-# Create directory for the application
+# write a condtion directory created or not 
+
 mkdir /app &>>$LOGFILE
-VALIDATE $? "Creating directory '/app'"
 
-# Download catalogue artifact
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
-VALIDATE $? "Downloading catalogue artifact"
 
-# Move to application directory
+VALIDATE $? "downloading catalogue aritifact"
+
 cd /app &>>$LOGFILE
-VALIDATE $? "Moving to application directory"
 
-# Unzip catalogue artifact
+VALIDATE $? "moving into app directory"
+
 unzip /tmp/catalogue.zip &>>$LOGFILE
-VALIDATE $? "Unzipping catalogue artifact"
 
-# Install Node.js dependencies
+VALIDATE $? "unziping  catalogue"
+
 npm install &>>$LOGFILE
-VALIDATE $? "Installing Node.js dependencies"
 
-# Copy catalogue service file
-cp /home/centos/roboshop.shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+VALIDATE $? "installing dependencies"
+
+cp /home/centos/roboshop.shell/catalogue.service  /etc/systemd/system/catalogue.service &>>$LOGFILE
+ 
+
+
 VALIDATE $? "Copying catalogue service file"
 
-# Reload systemd daemon
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Reloading systemd daemon"
 
-# Enable catalogue service
+VALIDATE $? "daemon reloaded"
+
 systemctl enable catalogue &>>$LOGFILE
-VALIDATE $? "Enabling catalogue service"
 
-# Start catalogue service
+VALIDATE $? "enable catalogue"
+
 systemctl start catalogue &>>$LOGFILE
-VALIDATE $? "Starting catalogue service"
 
-# Copy MongoDB repository file
+VALIDATE $? "started the catalogue"
+
 cp /home/centos/roboshop.shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-VALIDATE $? "Copying MongoDB repository file"
 
-# Install MongoDB shell
+VALIDATE $? "coping the mango repo"
+
 yum install mongodb-org-shell -y &>>$LOGFILE
-VALIDATE $? "Installing MongoDB shell"
 
-# Load catalogue data into MongoDB
+VALIDATE $? "installing mango clint"
+
 mongo --host mongodb.joinsankardevops.online </app/schema/catalogue.js &>>$LOGFILE
-VALIDATE $? "Loading catalogue data into MongoDB"
 
-# Print completion message
-echo "MongoDB installation and configuration completed successfully."
+VALIDATE $? "loading catalogue data in mangodb"
