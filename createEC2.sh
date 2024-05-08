8302702b81c13b7ba41a1f524746c3872a530883
sankar <sankargv17999@gmail.com>
2024-05-08 20:48:52 +0530

    creating the instance using iam and ading r53 records

--- createEC2.sh
+++ createEC2.sh
@@ -3,6 +3,7 @@
 NAMES=("mongodb" "redis" "mysql" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 IMAGE_ID="ami-0f3c7d07486cad139"
 SECURITY_GROUP_Id="sg-0b9372a5427937029"
+DOMAIN_NAME=joinsankardevops.online 
 
 for i in "${NAMES[@]}"
 do
@@ -14,9 +15,27 @@ do
 
     echo "Creating $i instance"
 
-    aws ec2 run-instances \
+   IP_ADDRESS=$(aws ec2 run-instances \
         --image-id "$IMAGE_ID" \
         --instance-type "$INSTANCE_TYPE" \
         --security-group-ids "$SECURITY_GROUP_Id" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-done
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"  | jq -r '.Instances[0].PrivateIpAddress')
+
+      echo "created $i instance: $IP_ADDRESS"
+
+      aws route53 change-resource-record-sets --hosted-zone-id ZZ05207513DMEHXL2N1MM2 --change-batch '
+{
+            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+}}]
+}
+'
+
+
+done 
